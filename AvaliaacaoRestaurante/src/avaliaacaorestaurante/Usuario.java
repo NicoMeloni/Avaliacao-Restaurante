40cfb32fa041c07f83484b9ebf8063a87639f622
NicoMeloni <150100175+NicoMeloni@users.noreply.github.com>
2025-01-21 19:44:06 -0300

    Add files via upload

---
+++ AvaliaacaoRestaurante/src/avaliaacaorestaurante/Usuario.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package avaliaacaorestaurante;
+
+/**
+ *
+ * @author Nicopau
+ */
+
+public class Usuario {
+    protected String usuario, senha, dataCadastro;
+    protected int idUsuario;
+    
+    public Usuario(String usuario, String senha) {
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+    
+    public Usuario(String usuario, String senha, int idUsuario, String dataCadastro) {
+        this.usuario = usuario;
+        this.senha = senha;
+        this.idUsuario = idUsuario;
+        this.dataCadastro = dataCadastro;
+    }
+    
+    
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    
+    
+}
